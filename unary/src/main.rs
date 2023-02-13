70cbf88c2c026a628e0cd0aa13c1c7a27ed57a13
mrghosti3 <mysteriousghst@gmail.com>
2023-02-13 21:48:14 +0200

    Impoved solution thanks to Kalwyn
    Base point: https://www.codingame.com/training/easy/unary/solution?id=4858224

--- unary/src/main.rs
+++ unary/src/main.rs
@@ -1,39 +1,7 @@
-use std::{io, fmt::Display};
-
 macro_rules! read_line {
     ($x:expr) => {
-        io::stdin().read_line($x).unwrap()
+        std::io::stdin().read_line($x).unwrap()
     };
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum BinaryE {
-    One,
-    Zero,
-    Empty,
-}
-
-impl BinaryE {
-    fn is_empty(&self) -> bool {
-        matches!(self, Self::Empty)
-    }
-}
-
-#[derive(Debug)]
-struct BinaryS {
-    btype: BinaryE,
-    count: u8,
-}
-
-impl Display for BinaryS {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let count_enc = "0".repeat(self.count as usize);
-        match self.btype {
-            BinaryE::One => write!(f, "0 {}", count_enc),
-            BinaryE::Zero => write!(f, "00 {}", count_enc),
-            BinaryE::Empty => write!(f, "")
-        }
-    }
 }
 
 /**
@@ -45,39 +13,33 @@ fn main() {
     read_line!(&mut input_line);
     let message = input_line.trim_matches('\n').to_string();
 
-    let bin: String = message.bytes().map(|c| format!("{:07b}", c)).collect();
+    let binding = message.bytes().map(|c| format!("{:07b}", c)).collect::<String>();
+    let bin_bytes = binding.as_bytes();
 
-    let mut b_encoded: BinaryS = BinaryS {
-        btype: BinaryE::Empty,
-        count: 0,
-    };
+    let mut last_char = bin_bytes[0];
 
-    let mut out_str = String::new();
-
-    for b in bin.bytes() {
-        let btype = match b {
-            // Case for '1'
-            0x31 => BinaryE::One,
-            // Case for '0'
-            0x30 => BinaryE::Zero,
-            _ => unreachable!()
-        };
-
-        if b_encoded.btype == btype {
-            b_encoded.count += 1;
-        } else {
-            out_str.push_str(&b_encoded.to_string());
-            if !b_encoded.btype.is_empty() {
-                out_str.push(' ');
-            }
-            b_encoded = BinaryS { btype, count: 1 }
-        }
+    match bin_bytes[0] {
+        0x30 => { print!("00 "); }
+        0x31 => { print!("0 "); }
+        _ => unreachable!(),
     }
 
-    out_str.push_str(&b_encoded.to_string());
+    for i in 0..bin_bytes.len() {
+        if bin_bytes[i] != last_char {
+            last_char = bin_bytes[i];
+            match bin_bytes[i] {
+                0x30 => { print!(" 00 "); }
+                0x31 => { print!(" 0 "); }
+                _ => unreachable!(),
+            }
+        }
+
+        print!("0");
+    }
+
 
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
 
-    println!("{}", out_str);
+    println!();
 }
